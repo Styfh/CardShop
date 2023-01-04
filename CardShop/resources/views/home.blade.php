9a197256b0ddfd47b8e09872937cb54cd51d4ecd
Styfh <stefan.senjaya145@gmail.com>
2023-01-04 13:50:45 +0700

    Added basic series display in home page

--- CardShop/resources/views/home.blade.php
+++ CardShop/resources/views/home.blade.php
@@ -2,10 +2,36 @@
 
 @section('title', 'Home')
 
+@section('style')
+<style>
+
+    main{
+        padding: 5rem 5%;
+    }
+
+    .series-card{
+        width: 16rem;
+    }
+
+    .series-img{
+        width: 100%;
+        height: 6rem;
+    }
+</style>
+@endsection
+
 @section('content')
 
-@foreach ($series_array as $series)
-
-@endforeach
+<div class="row row-cols-4">
+    @foreach ($series_array as $series)
+    <div class="col mx-auto">
+        <div class="card series-card mx-auto mb-3">
+            <a href="" class="btn">
+                <img src="{{asset("storage/$series->image")}}" class="series-img">
+            </a>
+        </div>
+    </div>
+    @endforeach
+</div>
 
 @endsection
