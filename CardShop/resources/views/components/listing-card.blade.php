a1d20d3a3e820853e55260123ecd6561615fee9e
Styfh <stefan.senjaya145@gmail.com>
2023-01-05 20:42:36 +0700

    Added rp. in price of product-card

--- CardShop/resources/views/components/listing-card.blade.php
+++ CardShop/resources/views/components/listing-card.blade.php
@@ -4,7 +4,7 @@
         <div class="card-body">
           <h5 class="card-title">{{ $listing->name }}</h5>
           <p class="card-text">{{ $listing->series->name }}</p>
-          <p class="card-text">{{ $listing->individual_price }}</p>
+          <p class="card-text">Rp. {{ $listing->individual_price }}</p>
         </div>
     </div>
 </a>
