27228fec7185e0ad438607629079f3e9f30a0da7
Styfh <stefan.senjaya145@gmail.com>
2023-01-10 20:10:12 +0700

    Added cart in migration on models

--- CardShop/app/Models/Cart.php
+++ CardShop/app/Models/Cart.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'listing_id',
+        'quantity'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function listing(){
+        return $this->belongsTo(Listing::class);
+    }
 }
