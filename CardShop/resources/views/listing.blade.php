1093484e5b9ac063ad0b81907e455e78a88a1a1b
Styfh <stefan.senjaya145@gmail.com>
2023-01-10 16:37:34 +0700

    Listing detail view done

--- CardShop/resources/views/listing.blade.php
+++ CardShop/resources/views/listing.blade.php
@@ -7,6 +7,11 @@
     main{
         padding: 5rem 5%;
     }
+
+    .listing-detail{
+        padding: 2rem;
+    }
+
 </style>
 @endsection
 
@@ -18,6 +23,23 @@
 
     <div>
         <img src="{{ asset("storage/$listing->image") }}">
+    </div>
+
+    <div class="listing-detail">
+        <strong>
+            <p class="card-text">IDR {{ $listing->individual_price }}</p>
+        </strong>
+        <p class="card-text">{{ $listing->series->name }}</p>
+        <p class="card-text">{{ $listing->description }}</p>
+        <p class="card-text">{{ $listing->category->name }}</p>
+
+        <form action="/cart/add" method="POST">
+            @csrf
+            <div class="d-flex flex-row">
+                <input type="number" name="amount" class="form-control">
+                <input type="submit" value="Add to Cart" class="form-control btn btn-primary w-50 mx-3">
+            </div>
+        </form>
     </div>
 
 </div>
